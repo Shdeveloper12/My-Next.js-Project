2ac4a7530d61d091b61acf8a468ba296eb1b3755
Shdevloper <shafayet237@gmail.com>
2025-07-26 23:42:43 +0600

    added home button on not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,10 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function NotFound() {
     return (
-        <div className="text-center">
+        <div className="text-center mt-5 space-y-4">
             <h1>404 - Not Found</h1>
             <p>The page you are looking for does not exist.</p>
+            <Link href="/"  className='btn btn-outline'>Back to home</Link>
         </div>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div>
+  <div className="text-center text-4xl mt-12">
     <h1>Welcome to My Next.js App</h1>
     
   </div>
