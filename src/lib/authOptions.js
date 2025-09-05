3afa1e2b7c0c4289c8a3272cac0ac3d03897a9e2
Shdevloper <shafayet237@gmail.com>
2025-09-05 20:31:47 +0600

    added gitHub login

--- src/lib/authOptions.js
+++ src/lib/authOptions.js
@@ -1,6 +1,7 @@
 import dbConnect, { collectionNames } from "@/lib/dbConnect";
 import CredentialsProvider from "next-auth/providers/credentials"
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 export const authOptions = {
     providers: [
         
@@ -42,6 +43,10 @@ export const authOptions = {
    GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  }),
+   GitHubProvider({
+    clientId: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET
   })
 ],
 callbacks : {
